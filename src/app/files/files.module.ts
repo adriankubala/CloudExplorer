d9027ec9c56dcb75f5fa2afedf61f4b0765633fc
Adrian Kubała <adrian.kubala@gmail.com>
2019-03-31 18:53:24 +0200

    Create files module

---
+++ src/app/files/files.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { FilesComponent } from './files.component';
+
+@NgModule({
+  declarations: [FilesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FilesModule { }
