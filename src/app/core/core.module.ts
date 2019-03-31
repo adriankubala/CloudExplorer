9ab56298f1b595d78e8ea31e67a8d61337d8c772
Adrian Kubała <adrian.kubala@gmail.com>
2019-03-31 19:48:51 +0200

    Create mock files

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { MOCK_FILES } from '../shared/injection-tokens';
+import { mockFiles } from '../shared/mock-files';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    { provide: MOCK_FILES, useValue: mockFiles },
   ]
 })
 export class CoreModule { }
